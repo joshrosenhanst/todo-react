fb52fc1aa4f881a46582a06f167ad2f6ddba5580
Josh mac <joshrh88@gmail.com>
2018-05-17 17:41:35 -0400

    update general css

--- todo/src/TodoEntry.js
+++ todo/src/TodoEntry.js
@@ -15,9 +15,14 @@ class TodoEntry extends React.Component {
     render() {
         return (
             <div className="Todo-entry">
-                <input type="text" placeholder="Add a todo item" 
-                    onKeyPress={this.handleKeyPress}
-                />
+                <div className="control has-icons-left">
+                    <input type="text"  className="input" placeholder="Add a todo item" 
+                        onKeyPress={this.handleKeyPress}
+                    />
+                    <span className="icon is-left">
+                        <i className="fas fa-plus"></i>
+                    </span>
+                </div>
             </div>
         );
     }
