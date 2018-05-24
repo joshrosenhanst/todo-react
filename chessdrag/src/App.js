e1e1288355d43a1558c2044cdaeaafec71aabae6
Josh VM <joshrh88@gmail.com>
2018-05-24 14:08:58 -0400

    update chessdrag initial template and add bulma/fontawesome support

--- chessdrag/src/App.js
+++ chessdrag/src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <section className="section">
+          <div className="container">
+            <h1 className="title">Chess Drag Tutorial App</h1>
+            <p className="subtitle icon"><i class="fas fa-chess-rook"></i></p>
+          </div>
+        </section>
       </div>
     );
   }
